14a3f407a33b4b3f33fc34816d2bcc8b70a6a90c
Martin <martin@flesk.es>
2022-03-31 17:44:54 +0200

    mein test commit

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 8000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/schnee', (req, res) => {
+  res.send('Hello Schnee!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
