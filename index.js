cc7b6777ba36ec2549125b18caa239c30a2661c5
Martin <vsc@flesk.es>
2022-04-01 08:21:11 +0200

    update

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require("express");
+const multer = require("multer");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-app.get('/schnee', (req, res) => {
-  res.send('Hello Schnee!')
-})
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${file.originalname}-${+Date.now()}`);
+  },
+});
+
+const app = express();
+const port = 8000;
+const upload = multer({ storage });
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.post("/upload-profile-pic", upload.single("profile_pic"), (req, res) => {
+  console.log(req.file);
+  res.send("profile pic uploaded");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
